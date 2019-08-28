3781694465885d983ae43bcdcdf9a55e301065f5
Infatzmut <fabrizzio1996@gmail.com>
2019-08-28 13:18:56 -0500

    all working

--- js/detalle.js
+++ js/detalle.js
@@ -2,16 +2,30 @@
 
 $(document).ready(function () {
 
-  //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
+  
+  
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
-
-  //Guarda el resultado en una variable
-
-  //Busca el elemento en el arreglo
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
+  $.ajax({
+    url:'info.json'
+  }).done(function(resp){
+    //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
+    var id = location.search.match(/id=(\d)*/)[1]
+    //Guarda el resultado en una variable
+    eventos = resp.eventos;
+    //Busca el elemento en el arreglo
+    evento = eventos.find(function(element){
+      return element.id==id
+    })
+     //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = `<h2>${evento.nombre}</h2>
+              <p>Fecha: ${evento.fecha} - ${evento.lugar}</p>
+              <p>${evento.descripcion}</p>
+              <p>Invitados: ${evento.invitados}</p>
+              <p>Precio: ${evento.precio}</p>` //Completa este HTML con los demas atributos del evento
 
   //Modifica el DOM agregando el html generado dentro del div con id=evento
-
+  document.getElementById("evento").innerHTML=html;
+  document.getElementById("evento").setAttribute("class","ev2")
+  })
 });
--- js/registro.js
+++ js/registro.js
@@ -1,7 +1,41 @@
 // Hemos omitido los acentos en los comentarios por compatibilidad
-
 function validar(formulario) {
 
-  //Expresion regular del correo
+  if (formulario.nombres.value.trim().length == 0) {
+    alert("Nombre obligatorio");
+    return false;
+  }
 
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(formulario.email.value)) {
+    alert("Email inválido");
+    return false;
+  }
+
+  if (formulario.contrasena.value.trim().length == 0) {
+    alert("Contraseña obligatorio");
+    return false;
+  }
+  if (formulario.contrasena.value.trim().length < 7) {
+    alert("Contraseña debe ser mas de 7 caracteres");
+    return false;
+  }
+
+
+  if (formulario.contrasena.value != formulario.confirmacion.value) {
+    alert("Confirmación no coincide");
+    return false;
+  }
+
+  if (formulario.tipo.value == "-1") {
+    alert("Seleccione un tipo de usuario");
+    return false;
+  }
+
+  if (!formulario.acepto.checked) {
+    document.getElementById("errorAcepto").innerHTML ="Debe seleccionar la casilla para continuar"
+    return false;
+  }
+
+  return false;
 }
